b1f15548fca7af60a246d63135a7ea5fdf84f8b5
nhotu2004 <nguyennhotu69@gmail.com>
2023-12-14 11:33:01 +0700

    gg

--- CHTPham/dangnhap.cs
+++ CHTPham/dangnhap.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 
 namespace CHTPham
 {
     public partial class dangnhap : Form
+     
+       
     {
+        SqlConnection sqlcon = null;
+
         public dangnhap()
         {
             InitializeComponent();
@@ -25,12 +30,41 @@ namespace CHTPham
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text == "nhotu" && textBox2.Text == "123")
+            if (sqlcon == null) 
             {
-                Form2 hello = new Form2();
-                hello.Show();
-                this.Hide();
+                sqlcon = new SqlConnection(@"Data Source=LAB1-MAY04\MISASME2022;Initial Catalog=DANGNHAP;Integrated Security=True"); 
             }
+            if (sqlcon.State == ConnectionState.Closed)
+            {     
+                sqlcon.Open();
+            }
+            string tk = textBox1.Text.Trim();
+            string mk = textBox2.Text.Trim();
+
+            SqlCommand sqlcmd = new SqlCommand();
+            sqlcmd.CommandType = CommandType.Text;
+            sqlcmd.CommandText = "SELECT * FROM nguoidungtaikhoan WHERE Nguoidung = '"+ tk +"' AND Matkhaunguoidung = '" + mk + "'";
+
+            sqlcmd.Connection = sqlcon;
+
+            SqlDataReader data = sqlcmd.ExecuteReader();
+            if (data.Read() == true)
+            {
+                MessageBox.Show("Đăng Nhập Thành Công");
+            }
+            else
+            {
+                MessageBox.Show("Đăng Nhập Thất Bại");
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
